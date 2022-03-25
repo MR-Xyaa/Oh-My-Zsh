8f4ce0d9368de7ab6317783ca1e1025b11d67778
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 07:31:02 +0700

    Create oh-my-zsh.sh

---
+++ module/oh-my-zsh.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#install project
+figlet Install Bahan | lolcat
+sleep 2
+
+apt update && apt install git -y
+git clone https://github.com/remo7777/T-Header.git
+
+figlet selesai | lolcat
+sleep 1
+
+cd T-Header/
+sleep 1
+
+# tampilan
+clear
+toilet -f big -F gay SpamCall 
+echo "=============================================" | lolcat
+echo "[•] Author : MR-Xyaa                         "
+echo "[•] FB     : Xyaa Xyaa                         "
+echo "[•] GitHub : github.com/MR-Xyaa              "
+echo "=============================================" | lolcat
+echo
+echo "=> 3x Spam Tunggu 20menit                    "
+echo "=> Jangan Berlebihan                         "
+echo "=> Follow FB dan Github Gw yakk^^            "
+echo
+echo "=============================================" | lolcat
+
+bash t-header.sh
